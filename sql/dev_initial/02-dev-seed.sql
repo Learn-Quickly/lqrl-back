83628eae19f0b70ae894bac57513590bfb6bcd18
advanced-user <stolbetskiy.yura@gmail.com>
2024-04-03 20:22:39 +0300

    small fix

--- sql/dev_initial/02-dev-seed.sql
+++ sql/dev_initial/02-dev-seed.sql
@@ -1,4 +1,6 @@
 -- User demo1
--- INSERT INTO "user" 
---     (username, cid, ctime, mid, mtime) VALUES 
---     ('demo1',  0,   now(), 0,   now());
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+INSERT INTO "user" 
+    (username, pwd, pwd_salt,           token_salt,         cid, ctime, mid, mtime) VALUES 
+    ('demo1',  '',  uuid_generate_v4(), uuid_generate_v4(), 0,   now(), 0,   now());
